8c28dbc84d3570a7cb2e411ea527f7906442c0da
Haokai Xuan <76743474+haokai-xuan@users.noreply.github.com>
2025-03-22 17:34:35 -0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-                # f"{brand} {category} product packaging canada",
-                # f"{brand} {category} product front canada",
-                # f"{brand} {category} package canada"
